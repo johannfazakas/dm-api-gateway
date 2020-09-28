4173cce9dba88a166cf13fbb5231be3d27850fef
Johann Fazakas <johannfazakas@gmail.com>
2020-09-28 18:57:36 +0300

    "initialized zuul spring boot service with routing configured to account service"

---
+++ src/main/java/ro/decision/maker/api/gateway/routing/ZuulConfiguration.java
@@ -0,0 +1,9 @@
+package ro.decision.maker.api.gateway.routing;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+
+@EnableZuulProxy
+@Configuration
+public class ZuulConfiguration {
+}
